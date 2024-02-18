2955651846d5fc7c855f7a6c3294dafe7600ba15
VitaliiDuduka <122724379+VitaliiDuduka@users.noreply.github.com>
2024-02-18 12:04:22 +0100

    Tasks detailed

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,15 @@
-import { useState, useEffect } from 'react';
-import Table from './components/Table'
-import './App.css';
+import { useState, useEffect } from "react";
+import Table from "./components/Table";
+import "./App.css";
 
 type User = {
   id: string;
   name: string;
-
-}
+};
 
 function App() {
   const [users, setUsers] = useState([] as User[]);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     fetchUsers();
@@ -18,40 +17,40 @@ function App() {
 
   const fetchUsers = async () => {
     try {
-      const response = await fetch('http://localhost:9000/users');
+      const response = await fetch("http://localhost:9000/users");
       const data = await response.json();
       setUsers(data);
     } catch (error) {
-      console.error('Error fetching users:', error);
+      console.error("Error fetching users:", error);
     }
   };
 
   const addUser = async () => {
     try {
-      const response = await fetch('http://localhost:9000/users', {
-        method: 'POST',
+      const response = await fetch("http://localhost:9000/users", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ name: inputValue }),
       });
       const newUser = await response.json();
       setUsers([...users, newUser]);
-      setInputValue('');
+      setInputValue("");
     } catch (error) {
-      console.error('Error adding user:', error);
+      console.error("Error adding user:", error);
     }
   };
 
-  console.log(users)
+  console.log(users);
   return (
-      <div className="App">
-        <h1 className="text-3xl font-bold underline text-red-600 my-10">
-          RecruitIT
-        </h1>
-        
-        <Table />
-      </div>
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-600 my-10">
+        RecruitIT
+      </h1>
+
+      <Table />
+    </div>
   );
 }
 
